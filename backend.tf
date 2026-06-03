0da01e478677c4cadd248b3f0d26a9ccdc3029f4
Ella <imohellanneamaka@gmail.com>
2026-06-03 13:33:25 +0100

    CICD Modified

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-tf-state-backend-22becd287"
+    bucket = "pod15-my-tf-state-backend-22becd287"
     key    = "prodution/terraform.tfstate"
     region = "us-east-1"
   }
